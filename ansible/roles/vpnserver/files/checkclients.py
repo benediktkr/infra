6663a2a9fbebd9858bc65f21b17b161b0bef3417
Benedikt Kristinsson <benedikt@inventati.org>
2020-01-25 20:09:36 +0100

    bugfixing

--- ansible/roles/vpnserver/files/checkclients.py
+++ ansible/roles/vpnserver/files/checkclients.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python2.7
 
+# This is a really shitty script
+# And i know it.
+# It runs python2.7 because theres a bug with sudoisbot in python3
+# its not meant to be a great script
+# might get around to fixing it at some point
+# or not
+
 from sudoisbot import sendmsg
 
 expected_clients = ["wifi001", "wifi002"]
@@ -23,21 +30,17 @@ if __name__ == "__main__":
         else:
             raise
 
-    print last
     s_last = set(last)
     s_conn = set(expt_connected)
     s_expt = set(expected_clients)
 
-    if s_last == s_expt:
-        # all expected
-        pass
-    else:
+    if s_last != s_conn:
         missing = s_expt - s_conn
         new = s_conn - s_last
 
         if missing:
             # check if it was missing last time
-            if s_last != missing:
+            if s_last != s_conn:
                 for client in missing:
                     sendmsg.send_to_me("{} has disconnected".format(client))
         if new:
