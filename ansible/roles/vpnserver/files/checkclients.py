81bb3ef7a5db9a474e151978643ec0daf8e7eb45
Benedikt Kristinsson <benedikt@inventati.org>
2020-04-25 09:31:24 +0200

    some python3 stuff

--- ansible/roles/vpnserver/files/checkclients.py
+++ ansible/roles/vpnserver/files/checkclients.py
@@ -1,16 +1,26 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3.8
 
 # This is a really shitty script
 # And i know it.
-# It runs python2.7 because theres a bug with sudoisbot in python3
-# its not meant to be a great script
-# might get around to fixing it at some point
-# or not
 
 from sudoisbot import sendmsg
+from sudoisbot.common import getconfig
 
-expected_clients = ["wifi001", "wifi002"]
+config = getconfig()['checkclients']
+
+#expected_clients = ["wifi001", "wifi002"]
+expected_clients = config['expected_clients']
 statefile = "/tmp/vpnclients.txt"
+
+def notify(client_name, action):
+    try:
+        sendto = config['notify'][client_name]
+        for a in sendto:
+            sendmsg.send_msg(a, f"`{client}` has {action}")
+    except KeyError:
+        sendmsg.send_to_me(f"new client `{client}` has `{action}`")
+
+
 
 if __name__ == "__main__":
     with open("/etc/openvpn/openvpn-status.log") as f:
@@ -42,10 +52,10 @@ if __name__ == "__main__":
             # check if it was missing last time
             if s_last != s_conn:
                 for client in missing:
-                    sendmsg.send_to_me("{} has disconnected".format(client))
+                    notify(client, "disonnected")
         if new:
             for client in new:
-                sendmsg.send_to_me("{} has connected".format(client))
+                notify(client, "connected")
                 #print "{} conected".format(client)
 
     with open(statefile, 'w') as f:
