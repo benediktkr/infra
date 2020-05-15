6a3a44951f1dde1205b8d55a486d3af416a95cb6
Benedikt Kristinsson <benedikt@inventati.org>
2020-05-16 01:13:37 +0200

    fucking bug, crashed when a client with no hostname connected

--- ansible/roles/sudoisbot/files/unifi.py
+++ ansible/roles/sudoisbot/files/unifi.py
@@ -22,12 +22,28 @@ def get_session(controller, username, password):
     return s
 
 def get_clients(session, controller):
+    # should just stuff the controller url in the session object
     clients = session.get(f"{controller}/api/s/default/stat/sta")
     clients.raise_for_status()
     return clients.json()['data']
 
+def get_client_names(session, controller):
+    names = list()
+    try:
+        for client in get_clients(session, controller):
+            try:
+                name = client.get('hostname', client['ip'])
+                names.append(name)
+            except KeyError:
+                # device has neither ip nor hostname
+                # something fucky is happening
+                print(f"weird client on unifi: {client}")
+        return names
+    except requests.exceptions.ConnectionError:
+        raise
 
 def people_home(session, controller):
+    home = set()
     try:
         with open(people_json, 'r') as f:
             people = json.loads(f.read())
@@ -35,16 +51,12 @@ def people_home(session, controller):
         people = dict()
         #raise
 
-    try:
-        home = set()
-        for client in get_clients(session, controller):
-            hostname = client['hostname']
-            if hostname in people:
-                home.add(people[hostname])
+    names = get_client_names(session, controller)
+    for name in names:
+        if name in people:
+            home.add(people[name])
+    return home
 
-        return home
-    except requests.exceptions.ConnectionError:
-        raise
 
 if __name__ == "__main__":
     import sys
@@ -53,5 +65,9 @@ if __name__ == "__main__":
     password = sys.argv[3]
     print(f"controller: {controller}")
     session = get_session(controller, username, password)
-    for client in get_clients(session, controller):
-        print(f"* {client['hostname']}")
+    print("connected:")
+    for name in get_client_names(session, controller):
+        print(f"* {name}")
+    print("home:")
+    for name in people_home(session, controller):
+        print(f"* {name}")
