893f43e2092d5f62039ae762a3815c726a816e12
Benedikt Kristinsson <benedikt@lokun.is>
2020-08-26 18:27:21 +0200

    cloudflare

--- tf/stack.tf
+++ tf/stack.tf
@@ -1,7 +1,13 @@
-variable "do_token" {
-}
+variable "do_token" {}
+
+variable "cf_token" {}
+variable "cf_email" {}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
+provider "cloudflare" {
+  email     = var.cf_email
+  api_key = var.cf_token
+}
