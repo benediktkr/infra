709315b5a3910f3ca9d1ac987699ab0fa71f178b
Benedikt Kristinsson <benedikt@lokun.is>
2020-08-26 18:19:49 +0200

    sensors role

---
+++ ansible/roles/sensors/files/dht.c
@@ -0,0 +1,121 @@
+/*
+ *  dht.c:
+ *	read temperature and humidity from DHT11 or DHT22 sensor
+ *
+ * depends on 'wiringpi' apt package
+ * based on:  http://www.uugear.com/portfolio/read-dht1122-temperature-humidity-sensor-from-raspberry-pi/
+ */
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_TIMINGS	85
+
+int data[5] = { 0, 0, 0, 0, 0 };
+
+uint8_t digital_read_and_print(int dht_pin) {
+    int digital = digitalRead(dht_pin);
+    /*printf("digitalRead: %i\n", digital);*/
+    return digital;
+}
+
+void print_json(int dht_pin) {
+    uint8_t laststate	= HIGH;
+    uint8_t counter	= 0;
+    uint8_t j	        = 0, i;
+
+    data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+
+    /* pull pin down for 18 milliseconds */
+    pinMode(dht_pin, OUTPUT);
+    digitalWrite(dht_pin, LOW);
+    delay(18);
+
+    /* prepare to read the pin */
+    pinMode(dht_pin, INPUT);
+    printf("%i\n", laststate);
+
+    /* detect change and read data */
+    for (i = 0; i < MAX_TIMINGS; i++) {
+        counter = 0;
+
+        while (digital_read_and_print(dht_pin) == laststate) {
+            counter++;
+            delayMicroseconds(1);
+            if (counter == 255)  {
+                printf("inner 255 at %i\n", i);
+                break;
+            }
+        }
+        laststate = digitalRead(dht_pin);
+        printf("%i\n", laststate);
+
+        if (counter == 255) {
+            printf("the second 255\n");
+            break;
+        }
+
+        /* ignore first 3 transitions */
+        if ( (i >= 4) && (i % 2 == 0) ) {
+            /* shove each bit into the storage bytes */
+            data[j / 8] <<= 1;
+            if (counter > 16) {
+                data[j / 8] |= 1;
+            }
+            printf("incrementing j\n");
+            j++;
+        }
+    }
+
+    /*
+     * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+     * print it out if data is good
+     */
+    if (j < 40) {
+        printf("{\"error\": \"only read %i bits\"}\n", j);
+        exit(10);
+    }
+    if (data[4] == ((data[0]+data[1]+data[2]+data[3]) & 0xFF)) {
+        float h = (float)((data[0] << 8) + data[1]) / 10;
+        if (h > 100) {
+            h = data[0];	// for DHT11
+        }
+        float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+        if (c > 125) {
+            c = data[2];	// for DHT11
+        }
+        if (data[2] & 0x80) {
+            c = -c;
+        }
+        printf("{\"humidity\": %.1f, \"temp\": %.1f}\n", h, c);
+    } else  {
+        printf("{\"error\": \"checksum\"}\n" );
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (wiringPiSetup() == -1) {
+        exit(1);
+    }
+    if (argc != 2) {
+        printf("{\"error\": \"usage: ./dht DHT_PIN\"}\n" );
+        exit(2);
+    }
+    /* dht_pin 3 is GPIO-22 */
+    int dht_pin = 0;
+    if (sscanf(argv[1], "%i", &dht_pin) != 1) {
+        printf("{\"error\": \"parsing DHT_PIN failed\"}\n");
+        exit(3);
+    }
+
+    if (dht_pin == 0) {
+        printf("{\"error\": \"DHT_PIN is still 0\"}\n");
+        exit(4);
+    }
+
+
+    print_json(dht_pin);
+    return(0);
+}
