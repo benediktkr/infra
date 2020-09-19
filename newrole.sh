58e8e90f6f991307b92a5b4db4a8b1bbb5b308c3
Benedikt Kristinsson <benedikt@lokun.is>
2020-09-19 21:14:15 +0200

    mainframe moore stuff

---
+++ newrole.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir roles/$1
+mkdir roles/$1/tasks
+mkdir roles/$1/templates
+mkdir roles/$1/handlers
+mkdir roles/$1/defaults
+
+touch roles/$1/tasks/main.yml
+touch roles/$1/tasks/$1.yml
+
+echo "---"                >> roles/$1/tasks/main.yml
+echo " - include: $1.yml" >> roles/$1/tasks/main.yml
+echo "   tags: $1"        >> roles/$1/tasks/main.yml
