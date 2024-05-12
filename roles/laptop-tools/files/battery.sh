8aa252ef02a21088fcfc3753d1155af55ca76989
Ben Kristinsson <ben@sudo.is>
2024-05-12 19:17:13 +0200

    laptop-tools: improving battery.sh script

--- roles/laptop-tools/files/battery.sh
+++ roles/laptop-tools/files/battery.sh
@@ -2,6 +2,7 @@
 
 BAT0=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2 }')
 BAT1=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage | awk '{ print $2 }')
+AC=$(cat /sys/class/power_supply/AC/online)
 
 echo '{'
 
@@ -11,6 +12,13 @@ echo '",'
 
 echo -ne '    "BAT1": "'
 echo -ne "${BAT1}"
-echo '"'
+echo '",'
+
+echo -ne '    "AC": '
+if [[ "$AC" == "1" ]]; then
+    echo "true"
+else
+    echo "false"
+fi
 echo '}'
 
